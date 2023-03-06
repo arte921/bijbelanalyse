c2df562f6cff8133f8236bc16c80082edf46e047
arte921 <ardjanve@gmail.com>
2023-03-06 20:18:46 +0000

    e

--- v3/index.js
+++ v3/index.js
@@ -3,6 +3,10 @@ import puppeteer from 'puppeteer';
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
-await page.goto('https://www.statenvertaling.net/nieuwe-testament.html');
+// await page.goto('https://www.statenvertaling.net/nieuwe-testament.html');
 
-await page.$$eval(`#inhoud > tbody > tr > td`, (e) => console.log(e))
+// await page.$$eval(`#inhoud > tbody > tr > td`, (e) => console.log(e))
+
+
+await page.goto('https://www.statenvertaling.net/nieuwe-testament.html');
+await page.$$eval(`#tekst > p`, (v) => console.log(v));
